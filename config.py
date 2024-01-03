a56d65c2cb5f0e3b1a9cc3a753e04f6a00c82f2b
twilight <malirahimi582@gmail.com>
2024-01-03 15:33:41 +0330

    - clean code

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
-screen_size = width, height = 800, 800  # size of whole screen
-center_dot_position = (400, 400)  # place of center dot
+SCREEN_SIZE = width, height = 800, 800  # size of whole screen
+CENTER_DOT_POSITION = (400, 400)  # place of center dot
 background_color = (224, 255, 255)  # light cyan
 center_dot_color = (47, 79, 79)  # dark slate gray
 screen_dots_color = (236, 30, 5)  # crimson
 line_draw_color = (0, 0, 128)  # Navy
+num_dots = 50  # number of dots
--- utils.py
+++ utils.py
@@ -2,47 +2,55 @@ import random
 import time
 import pygame
 import math
-from config import screen_size, center_dot_position, background_color, center_dot_color, screen_dots_color, \
-    line_draw_color
+from config import SCREEN_SIZE, CENTER_DOT_POSITION, background_color, center_dot_color, screen_dots_color, \
+    line_draw_color, num_dots
 
 
 def create_screen():
     pygame.init()
-    screen = pygame.display.set_mode(screen_size)
+    screen = pygame.display.set_mode(SCREEN_SIZE)
     screen.fill(background_color)
     pygame.display.update()
     return screen
 
 
 def create_center_dot(screen):
-    pygame.draw.circle(screen, pygame.Color(center_dot_color), center_dot_position, 10)
+    pygame.draw.circle(screen, pygame.Color(center_dot_color), CENTER_DOT_POSITION, 10)
     pygame.display.update()
 
 
-def draw_line(screen, end_pos):
+def _draw_line(screen, end_pos):
     pygame.draw.circle(screen, pygame.Color(screen_dots_color), end_pos, 5)
-    pygame.draw.line(screen, pygame.Color(line_draw_color), center_dot_position, end_pos, 2)
+    pygame.draw.line(screen, pygame.Color(line_draw_color), CENTER_DOT_POSITION, end_pos, 2)
     pygame.display.update()
     time.sleep(0.1)
 
 
-def distance(point1, point2):
-    return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
+# def _distance(point1, point2):
+#     """distance calculation with Pythagoras Formula"""
+#     return math.sqrt((point2[0] - point1[0]) ** 2 + (point2[1] - point1[1]) ** 2)
 
 
-def generate_random_dots(num_dots):
-    max_distance = math.sqrt(screen_size[0] ** 2 + screen_size[1] ** 2) / 2
+"""
+generates random angles,
+calculates coordinate points incrementally further
+from the center point using trigonometry
+"""
+
+
+def _generate_random_dots():
+    max_distance = math.sqrt(SCREEN_SIZE[0] ** 2 + SCREEN_SIZE[1] ** 2) / 2
     distance_increment = max_distance / num_dots
 
     dots = []
     for i in range(1, num_dots + 1):
         angle = random.uniform(0, 2 * math.pi)
         dist = distance_increment * i
-        x = center_dot_position[0] + dist * math.cos(angle)
-        y = center_dot_position[1] + dist * math.sin(angle)
+        x = CENTER_DOT_POSITION[0] + dist * math.cos(angle)
+        y = CENTER_DOT_POSITION[1] + dist * math.sin(angle)
 
-        x = max(0, min(screen_size[0], x))
-        y = max(0, min(screen_size[1], y))
+        x = max(0, min(SCREEN_SIZE[0], x))
+        y = max(0, min(SCREEN_SIZE[1], y))
 
         dots.append((int(x), int(y)))
 
@@ -50,6 +58,6 @@ def generate_random_dots(num_dots):
 
 
 def draw_dots(screen):
-    dots = generate_random_dots(50)
+    dots = _generate_random_dots()
     for dot in dots:
-        draw_line(screen, dot)
+        _draw_line(screen, dot)
