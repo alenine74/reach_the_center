84bece8602ac9531baa0cd2aa0fc5977460a7786
yasinsiadat <siadatnezhad2@gmail.com>
2024-01-03 13:55:23 +0330

    Save the image

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import time
+import pygame
 from utils import create_screen, create_center_dot, draw_dots
 
 def main():
@@ -6,7 +7,9 @@ def main():
     create_center_dot(screen)
     draw_dots(screen)
     time.sleep(5) 
+    
+    pygame.image.save(screen, "final_image.png")
+    print("Image saved as 'final_image.png'.")
 
 if __name__ == "__main__":
     main()
-
